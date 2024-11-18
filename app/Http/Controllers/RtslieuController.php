2c68693a8a0fbd64aa02d6931b2edeb33d827a24
Ibra Ndiaye <ibra789diaye@gmail.com>
2024-11-18 17:29:42 +0000

    tt

--- app/Http/Controllers/RtslieuController.php
+++ app/Http/Controllers/RtslieuController.php
@@ -1112,7 +1112,7 @@ public function rtsByBureatTemoin()
       $hs  = $this->lieuvoteRepository->nbHsByDepartement($departement_id);
       $inscrit = $this->lieuvoteRepository->sumByDepartements($departement_id);
       foreach ($rts as $key => $value) {
-        $rts[$key]->taux = round(($value->nb*100)/$votant,2);
+        $rts[$key]->taux = round(($votant*100)/$inscrit,2);
         $rts[$key]->taux =   $rts[$key]->taux.'%';
       }
       //dd($rts,$votant);
